24974b92a20022d4270fb14cbb76414b60c2f6b5
biloliddin937523553 <biloliddinlatipov0@gmail.com>
2024-02-26 21:54:16 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+let string = prompt('String typedagi malumot kiriting')
+let Number = prompt('Number typedagi malumot kiriting')
+let Boolean = prompt('Boolean typedagi malumot kiriting')
+
+alert('hamma malumotlarni togri kiritganingizga ishonchingizkomilmi?')
+
+let kiritish ='Siz kiritgan '
+let kiritis ='                                        Siz kiritgan '
+let kiritish2 ='                                           Siz kiritgan '
+let string1 = (' sozi string typega tegishli')
+let Number1 = (' sozi number typega tegishli')
+let Boolean1 = (' sozi Boolean typega tegishli')
+
+                                 
+
+alert(kiritish + string + string1 +
+    kiritis + Number + Number1 +
+    kiritish2 + Boolean + Boolean1);
